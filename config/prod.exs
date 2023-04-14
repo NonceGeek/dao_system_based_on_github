6966bf207df8a3751bd370e3caecdaf3f3fc0120
leeduckgo <albertschr@163.com>
2023-04-14 16:15:48 +0800

    update with links

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,9 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :dao_system_based_on_github, DAOSystemBasedOnGithubWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
